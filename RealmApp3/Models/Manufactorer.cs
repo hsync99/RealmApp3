33a4a9ff2bf4aadac89e116435430aa8cae60346
hsync99 <61741158+hsync99@users.noreply.github.com>
2021-04-11 16:57:18 +0600

    Dynamic params, barcode scan

--- RealmApp3/Models/Manufactorer.cs
+++ RealmApp3/Models/Manufactorer.cs
@@ -3,10 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace RealmApp3.Models
 {
     public class Manufactorer:RealmObject
     {
+        public int id = 4;
         public string name { get; set; }
     }
 }
